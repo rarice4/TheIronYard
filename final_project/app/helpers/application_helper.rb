48c0bdb1fcbc0c720cc18142ec99d4f322f0dd29
Robert Rice <rarice@g.cofc.edu>
2014-10-09 23:18:03 -0400

    user login links

--- final_project/app/helpers/application_helper.rb
+++ final_project/app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class, remote: true} 
 
   end
+
+  def user_login_links
+    if user_signed_in?
+      # (link_to "Edit Account", edit_user_registration_path)+
+      (link_to "Log Out", destroy_user_session_path, method: :delete)
+    else
+      link_to "Log In", new_user_session_path
+    end
+end
+
 end
